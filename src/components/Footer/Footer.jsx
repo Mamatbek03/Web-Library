cb6454996e50b2c09401d9f1b6b8f9bed6debeb5
BelekovDastan <kggoldenman@mail.ru>
2023-04-11 17:10:06 +0600

    Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,24 @@
 import React from "react";
 import "./Footer.css";
+import logo from "./media/logo.svg";
 
 const Footer = () => {
   return (
     <div id="footer">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos,
-      accusantium explicabo quas error tempora deserunt commodi eos expedita
-      fugit mollitia repudiandae, ab voluptas nemo obcaecati nobis ex est,
-      deleniti temporibus!
+      <div id="footer_left">
+        <img id="footer_logo" src={logo} alt="" />
+        <p>Количество книг: 0</p>
+      </div>
+      <div id="footer_center">
+        <p>Право обладания</p>
+        <p>Партнёрство</p>
+        <p>Контакты</p>
+        <p>О нас</p>
+      </div>
+      <div id="footer_right">
+        <input type="text" placeholder="Введите почту" />
+        <button>Подписаться</button>
+      </div>
     </div>
   );
 };
