a24a6f5d3db8ee75ee6029178aacd176a30c5e43
Mamatbek <kamytoev.2003@gmail.com>
2023-04-12 14:55:13 +0600

    add online read

--- src/Pages/ProductsPage/ProductsPage.jsx
+++ src/Pages/ProductsPage/ProductsPage.jsx
@@ -1,12 +1,40 @@
 import React, { useState } from "react";
 import { Document, Page } from "react-pdf/dist/esm/entry.webpack";
-import book from "./fistBook.pdf";
+import book from "./secondBook.pdf";
+import { Button } from "@mui/material";
 const ProductsPage = () => {
   const [totalPages, setTotalPages] = useState(null);
   const [pageNumber, setPageNumber] = useState(1);
   function onDocumentLoadSuccess({ numPages }) {
     setTotalPages(numPages);
   }
+  function removeTextLayerOffset() {
+    const textLayers = document.querySelectorAll(
+      ".react-pdf__Page__textContent textLayer"
+    );
+    const textLayers2 = document.querySelectorAll(
+      ".react-pdf__Page__annotations annotationLayer"
+    );
+    const textLayers3 = document.querySelector(".react-pdf__Page");
+    textLayers3.style.height = "1200px";
+    textLayers.forEach((layer) => {
+      const { style } = layer;
+      style.width = "0";
+      style.display = "none";
+      style.height = "0";
+      style.top = "0";
+      style.left = "0";
+      style.confirm = "";
+    });
+    textLayers2.forEach((layer) => {
+      const { style } = layer;
+      style.display = "none";
+      style.top = "0";
+      style.left = "0";
+      style.confirm = "";
+    });
+  }
+
   function changePage(offSet) {
     setPageNumber((prevPageNumber) => prevPageNumber + offSet);
   }
@@ -18,23 +46,29 @@ const ProductsPage = () => {
   }
   return (
     <div className="big">
-      <center className="small">
-        <Document file={book} onLoadSuccess={onDocumentLoadSuccess}>
-          <Page height="1200" pageNumber={pageNumber} />
+      <center style={{ overflow: "hidden" }} className="small">
+        <Document
+          className="doc"
+          file={book}
+          onLoadSuccess={onDocumentLoadSuccess}
+        >
+          <Page
+            className="page"
+            height="1200"
+            onLoadSuccess={removeTextLayerOffset}
+            pageNumber={pageNumber}
+          />
         </Document>
+      </center>
+      <center>
         <p style={{ marginTop: "0" }}>
           Page {pageNumber} of {totalPages}
         </p>
-        {pageNumber > 1 && <button onClick={prevPage}>Previous Page</button>}
+        {pageNumber > 1 && <Button onClick={prevPage}>Previous Page</Button>}
         {pageNumber < totalPages && (
-          <button onClick={nextPage}>Next Page</button>
+          <Button onClick={nextPage}>Next Page</Button>
         )}
       </center>
-      {/* <Document file={book} onLoadSuccess={onDocumentLoadSuccess}>
-        {Array.from(new Array(totalPages), (el, next) => (
-          <Page key={index + 1} pageNumber={index + 1} />
-        ))}
-      </Document> */}
     </div>
   );
 };
