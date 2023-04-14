c6ba3369585df7ae83cb8043c8911962075de521
BelekovDastan <kggoldenman@mail.ru>
2023-04-14 08:25:02 +0600

    footer end

--- src/Pages/FootersPage/Partnership/Partnership.jsx
+++ src/Pages/FootersPage/Partnership/Partnership.jsx
@@ -1,9 +1,70 @@
 import React from "react";
+import "./Partnership.css";
 
 const Partnership = () => {
   return (
-    <div>
-      Partnership <input type="text" />
+    <div id="partnership_main">
+      <div id="partnership_content">
+        <h2>Что мы можем предложить?</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae quo
+          commodi id similique at praesentium qui eos aliquid culpa officiis
+          quam rem adipisci iure architecto provident officia pariatur,
+          assumenda doloremque? Corporis totam quaerat minus porro fuga, ab
+          repellendus velit impedit officiis quisquam incidunt dignissimos quis
+          ullam! Veritatis in nulla quia voluptas! Exercitationem, velit? Modi
+          rem natus officia asperiores cupiditate? Repellendus qui perferendis
+          nam nemo ipsam, harum veritatis reiciendis aperiam reprehenderit
+          accusantium assumenda eum explicabo quis illo soluta totam? Cum in id
+          reiciendis incidunt aperiam molestiae officiis aliquam eum assumenda,
+          voluptate eveniet laboriosam dolor rerum ullam dignissimos magni iure
+          repudiandae totam ad. Quis assumenda laudantium fuga repellat at dolor
+          eos nam vitae ea eaque asperiores distinctio veritatis, consequatur
+          ullam, ipsam alias. Recusandae voluptas accusamus ea quisquam neque
+          ducimus officia porro, cupiditate culpa dolor aperiam voluptatem ab
+          facere laboriosam, quidem reprehenderit vitae fugit numquam nisi
+          reiciendis. Ullam doloribus corrupti recusandae libero architecto
+          autem voluptatibus ad voluptate debitis doloremque laudantium
+          molestiae inventore reiciendis iste aliquid, repellendus, tenetur est
+          quibusdam. Eligendi dignissimos consectetur, vel magnam temporibus
+          minus natus iure mollitia voluptas quasi perferendis ut voluptatem,
+          odio sed accusantium vero obcaecati aut nihil autem, suscipit
+          laudantium. Magni ad est neque magnam obcaecati fugit. Possimus,
+          voluptatum.
+        </p>
+      </div>
+      <div>
+        <fieldset id="fieldset_right">
+          <legend>Наши услуги</legend>
+          Keep calm and carry on your job Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Cumque provident officiis praesentium quae voluptate
+          quidem, exercitationem ipsa architecto, eius nihil natus. Neque in
+          consectetur a officia cum, corporis ea quos aspernatur quod, nulla
+          unde? Dolorem quasi neque quibusdam error mollitia eos adipisci
+          laudantium nostrum dolores accusantium at minima laboriosam, facere ex
+          ut quisquam magni earum, cupiditate iste quo ipsa voluptas perferendis
+          maxime dignissimos. Aut corrupti ipsa incidunt error repudiandae dicta
+          illo ducimus veritatis maiores dolor, nobis, excepturi laboriosam quos
+          enim. Ea assumenda amet, nostrum fugit aliquid expedita? Quo
+          repellendus assumenda commodi, sed aliquam perferendis saepe impedit
+          voluptate molestias numquam tempora? Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Maiores ut provident molestias id
+          placeat incidunt recusandae cumque magnam facere iure, dolor nihil
+          corporis modi qui quos ratione optio amet! Architecto odio, temporibus
+          dolor error eveniet dicta rerum quam vel accusantium maxime, totam
+          laboriosam veritatis minus saepe esse in praesentium et deleniti
+          asperiores debitis. Dolorem facere delectus vitae dolore voluptatibus
+          voluptatem enim ex numquam amet ullam possimus, expedita nesciunt
+          voluptates temporibus fugit hic accusantium ad, aliquid, porro
+          pariatur cum? Nesciunt velit corporis quisquam corrupti modi. Neque
+          facilis perferendis doloribus est autem nesciunt, optio commodi
+          temporibus tempora, repellendus deleniti facere odio odit.
+        </fieldset>
+        <fieldset id="fieldset_left">
+          Наш адрес: Табышалиева 29 Bishkek · 0501 619 690 Открыто ⋅ Закроется в
+          19:00
+        </fieldset>
+      </div>
     </div>
   );
 };
